bbfe7bad6e5519596363277ba269f8f275de551d
feheed <fmjuwaihel@gmail.com>
2022-01-16 10:16:59 +0300

    important element in body

--- ollivars-wand-shop/src/App.js
+++ ollivars-wand-shop/src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Component/Home";
 import NavBar from "./Component/NavBar";
-import Wands from "./Wands";
 import Products from "./Component/Products";
-import { useState } from "react";
 function App() {
   return (
     <div>
@@ -13,8 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Products" element={<Products />} />
       </Routes>
-
-      <div></div>
     </div>
   );
 }
